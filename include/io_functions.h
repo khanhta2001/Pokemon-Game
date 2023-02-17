3c0f8203980b5b5063daa949d4f0f9aa73c37150
khanhta2001 <ktran2@iwu.edu>
2023-02-17 00:47:28 -0600

    added pokemon_moves and added functions that convert json data to hashmap

--- include/io_functions.h
+++ include/io_functions.h
@@ -12,13 +12,13 @@ class io_functions{
 public:
     io_functions();
     ~io_functions();
-    std::map<std::string,std::map<std::string,std::string>> MovesData();
-    std::map<std::string,std::map<std::string,std::string>> PokeStats();
-    std::map<std::string,std::map<std::string,std::string>> TypeDamage();
+    std::map<std::string,std::map<std::string,std::string>> stats_info(const std::string& file_type);
+    std::map<std::string,std::map<std::string,std::string>> moves_info();
 private:
     std::string moves_file;
     std::string pokemon_stats;
     std::string type_damage;
+    std::string pokemon_moves;
 
 };
 #endif //GAME_IO_FUNCTIONS_H
--- src/io_functions.cpp
+++ src/io_functions.cpp
@@ -12,15 +12,24 @@ io_functions::io_functions() {
     io_functions::moves_file = "../../data/moves.json";
     io_functions::pokemon_stats = "../../data/pokemon_stats.json";
     io_functions::type_damage = "../../data/type_damage.json";
+    io_functions::pokemon_moves = "../../data/pokemon_moves.json";
 }
 
-io_functions::~io_functions() {
+io_functions::~io_functions() = default;
 
-}
+std::map<std::string,std::map<std::string,std::string>> io_functions::stats_info(const std::string& file_type) {
+    std::string file;
+    if (file_type == "type_damage"){
+        file = io_functions::type_damage;
+    }
+    else if (file_type == "poke_stats"){
+        file = io_functions::pokemon_stats;
+    }
+    else if (file_type == "moves_stats"){
+        file = io_functions::moves_file;
+    }
 
-std::map<std::string,std::map<std::string,std::string>> io_functions::MovesData() {
-    std::ifstream inputFile(io_functions::moves_file);
-
+    std::ifstream inputFile(file);
     // Read the contents of the file into a string
     std::string jsonStr((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
 
@@ -46,10 +55,11 @@ std::map<std::string,std::map<std::string,std::string>> io_functions::MovesData(
     return HashMap;
 }
 
+std::map<std::string, std::map<std::string, std::string>> io_functions::moves_info() {
+    std::string file = io_functions::pokemon_moves;
 
-std::map<std::string,std::map<std::string,std::string>> io_functions::PokeStats() {
-    std::ifstream inputFile(io_functions::pokemon_stats);
 
+    std::ifstream inputFile(file);
     // Read the contents of the file into a string
     std::string jsonStr((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
 
@@ -62,40 +72,7 @@ std::map<std::string,std::map<std::string,std::string>> io_functions::PokeStats(
         const rapidjson::Value& val = itr->value;
         std::map<std::string,std::string> map2;
         for (rapidjson::Value::ConstMemberIterator ptr = val.MemberBegin(); ptr != val.MemberEnd(); ++ptr) {
-            if (!ptr->value.IsInt()){
                 map2[ptr->name.GetString()] = ptr->value.GetString();
-            }
-            else{
-                map2[ptr->name.GetString()] = std::to_string(ptr->value.GetInt());
-            }
-        }
-        HashMap[itr->name.GetString()] = map2;
-    }
-
-    return HashMap;
-}
-
-std::map<std::string,std::map<std::string,std::string>> io_functions::TypeDamage() {
-    std::ifstream inputFile(io_functions::type_damage);
-
-    // Read the contents of the file into a string
-    std::string jsonStr((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
-
-    rapidjson::Document sd;
-    sd.Parse(jsonStr.c_str());
-
-    std::map<std::string,std::map<std::string,std::string>> HashMap;
-
-    for (rapidjson::Value::ConstMemberIterator itr = sd.MemberBegin(); itr != sd.MemberEnd(); ++itr) {
-        const rapidjson::Value& val = itr->value;
-        std::map<std::string,std::string> map2;
-        for (rapidjson::Value::ConstMemberIterator ptr = val.MemberBegin(); ptr != val.MemberEnd(); ++ptr) {
-            if (!ptr->value.IsInt()){
-                map2[ptr->name.GetString()] = ptr->value.GetString();
-            }
-            else{
-                map2[ptr->name.GetString()] = std::to_string(ptr->value.GetInt());
-            }
         }
         HashMap[itr->name.GetString()] = map2;
     }
